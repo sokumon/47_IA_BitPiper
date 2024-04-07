69e977396054128499e11ba7b894ecf91264d6ee
semisenioritis <atharvadhamankar44@gmail.com>
2024-04-07 11:50:27 +0530

    stitching bot with server

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/parse', methods=['POST'])
 def parse_json():
